fcb7b4b690cda5eb6c978c0a76980bec943c42e0
IamMaheshGurung <162683026+IamMaheshGurung@users.noreply.github.com>
2024-06-02 07:39:12 +0900

    rebuilding

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div className='Header'>
+        <h1 className='heading'>Easy Parking Japan</h1>
+        <h2 className='sub-heading'>Worry on other things, We will take care of your car</h2>
+      
+    </div>
+  )
+}
+
+export default Header
