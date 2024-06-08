461aa04d5573d47335a4d079d32a5b4a25874a7e
Mahesh <gurungmahesh167@gmail.com>
2024-06-09 05:22:07 +0900

    simple modification

--- src/Components/Practice.jsx
+++ src/Components/Practice.jsx
@@ -1,19 +1,30 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 const Practice = () => {
-    const [count, setCount] = useState(0)
-    useEffect(()=> {
-        setTimeout(()=>
-        {
-            setCount(count => count+1)
-        },2000)
-        
-    },[])
 
+    const choice = useRef()
+    
+    const [count, setCount] = useState(0);
+    const bClicked = () => {
+      setCount(parseInt(choice.current.value));
+      document.body.style.background = "blue";
+    };
+    const value = useRef(0);
+
+    useEffect(()=>{
+      value.current = value.current+1;
+    });
+
+   
   return (
     
     <div>
+        <input type="number" ref={choice}/>
+        <button onClick={bClicked}>Starting Number</button>
+        <button onClick={()=> setCount(count=> count+1)}>Plus</button>
         <h1> I have rendered {count} times</h1>
+        <button onClick={()=> setCount(count=> count-1)}>Minus</button>
+        <h1> {value.current}</h1>
       
     </div>
   )
